b0a84b6e047fbb3ac4373b6516e04fbf9225fa23
sryung <sryung1225@gmail.com>
2023-10-01 00:04:14 +0900

    [⚙] 테마 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
-import { createGlobalStyle } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import App from "./App";
+import { theme } from "./theme";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -74,8 +75,10 @@ const root = ReactDOM.createRoot(
 root.render(
   // <React.StrictMode>
   <RecoilRoot>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </RecoilRoot>
   // </React.StrictMode>
 );
