e8bbb26f0afa95d47cc84c5ad3ad7e46d786231f
sryung <sryung1225@gmail.com>
2023-10-16 19:03:37 +0900

    [🥁🎀] Header 삽입 및 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
 import Tv from "./Routes/Tv";
 import Search from "./Routes/Search";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tv" element={<Tv />} />
