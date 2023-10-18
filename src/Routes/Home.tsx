6fbe1c94e2ecefefcdd109e0642dceb47bf84fcf
sryung <sryung1225@gmail.com>
2023-10-18 23:44:20 +0900

    [🥁] TMDB API 연결 - api key는 env에서 관리 - data의 인터페이스 지정 후 useQuery에 전달

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,12 @@
+import { useQuery } from "react-query";
+import { IGetMoviesResult, getMovies } from "../api";
+
 function Home() {
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+  console.log(data, isLoading);
   return <h1>Home</h1>;
 }
 
